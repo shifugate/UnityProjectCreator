57b8ebbb045808611b8ee14041229914872a5ac9
mauricio ferreira <mauricioferreira@adeninteractive.com>
2025-04-11 19:43:36 +0100

    updated

---
+++ _Template/Packages/SoftMaskForUGUI/Runtime/ProjectSettings/UISoftMaskProjectSettings.cs
@@ -0,0 +1,195 @@
+#pragma warning disable CS0414
+using System.Linq;
+using Coffee.UISoftMaskInternal;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_MODULE_VR
+using UnityEngine.XR;
+#endif
+
+namespace Coffee.UISoftMask
+{
+    public class UISoftMaskProjectSettings : PreloadedProjectSettings<UISoftMaskProjectSettings>
+    {
+        private static bool s_UseStereoMock;
+
+        [Header("Setting")]
+        [Tooltip("Enable SoftMask globally.")]
+        [SerializeField]
+        internal bool m_SoftMaskEnabled = true;
+
+        [Tooltip("Enable stereo rendering for VR devices.")]
+        [SerializeField]
+        private bool m_StereoEnabled = true;
+
+        [Tooltip("Sensitivity of transform that automatically rebuilds the soft mask buffer.")]
+        [SerializeField]
+        private TransformSensitivity m_TransformSensitivity = TransformSensitivity.Medium;
+
+        [Header("Editor")]
+        [Tooltip("Hide the automatically generated components.\n" +
+                 "  - MaskingShapeContainer\n" +
+                 "  - TerminalMaskingShape")]
+        [SerializeField]
+        private bool m_HideGeneratedComponents = true;
+
+        [HideInInspector]
+        [SerializeField]
+        private ShaderVariantRegistry m_ShaderVariantRegistry = new ShaderVariantRegistry();
+
+        public static ShaderVariantRegistry shaderRegistry => instance.m_ShaderVariantRegistry;
+
+        public static ShaderVariantCollection shaderVariantCollection => shaderRegistry.shaderVariantCollection;
+
+        public static bool softMaskEnabled => instance.m_SoftMaskEnabled;
+
+        public static bool useStencilOutsideScreen => true;
+
+#if UNITY_MODULE_VR
+        public static bool stereoEnabled =>
+#if UNITY_EDITOR
+            Application.isPlaying &&
+#endif
+            softMaskEnabled && instance.m_StereoEnabled && XRSettings.enabled;
+#else
+        public static bool stereoEnabled => false;
+#endif
+
+        public static HideFlags hideFlagsForTemp => instance.m_HideGeneratedComponents
+            ? HideFlags.DontSaveInEditor | HideFlags.NotEditable | HideFlags.HideInHierarchy | HideFlags.HideInInspector
+            : HideFlags.DontSaveInEditor | HideFlags.NotEditable;
+
+        public static TransformSensitivity transformSensitivity
+        {
+            get => instance.m_TransformSensitivity;
+            set => instance.m_TransformSensitivity = value;
+        }
+
+        public static bool useStereoMock
+        {
+            get => s_UseStereoMock;
+            set
+            {
+                if (s_UseStereoMock == value) return;
+                s_UseStereoMock = value;
+                ResetAllSoftMasks();
+            }
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+#if UNITY_EDITOR
+            SetupSamplesForShaderVariantRegistry();
+            m_ShaderVariantRegistry.ClearCache();
+#endif
+        }
+
+        private static void ResetAllSoftMasks()
+        {
+            var softMasks = InternalListPool<SoftMask>.Rent();
+            var components = InternalListPool<IMaskable>.Rent();
+
+            foreach (var softMask in Misc.FindObjectsOfType<SoftMask>())
+            {
+                // #208: Accessing game object transform hierarchy before loading of scene has completed.
+                if (!softMask.gameObject.scene.isLoaded) continue;
+
+                softMask.GetComponentsInParent(true, softMasks);
+                if (1 < softMasks.Count) continue;
+
+                softMask.GetComponentsInChildren(true, components);
+                components.ForEach(c => c.RecalculateMasking());
+            }
+
+            InternalListPool<IMaskable>.Return(ref components);
+            InternalListPool<SoftMask>.Return(ref softMasks);
+        }
+
+#if UNITY_EDITOR
+        protected override void OnCreateAsset()
+        {
+            m_ShaderVariantRegistry.InitializeIfNeeded(this, "(SoftMaskable)");
+        }
+
+        protected override void OnInitialize()
+        {
+            m_ShaderVariantRegistry.InitializeIfNeeded(this, "(SoftMaskable)");
+        }
+
+        private void OnValidate()
+        {
+            ResetAllSoftMasks();
+            ResetAllHideFlags<MaskingShapeContainer>(hideFlagsForTemp);
+            ResetAllHideFlags<TerminalMaskingShape>(hideFlagsForTemp);
+        }
+
+        private void SetupSamplesForShaderVariantRegistry()
+        {
+#if UNITY_2023_2_OR_NEWER
+            const string tmpSupport = "TextMeshPro Support (Unity 6)";
+#else
+            const string tmpSupport = "TextMeshPro Support";
+#endif
+            m_ShaderVariantRegistry.RegisterSamples(new[]
+            {
+                // TextMeshPro Support
+                ("Hidden/TextMeshPro/Bitmap (SoftMaskable)", tmpSupport),
+                ("Hidden/TextMeshPro/Mobile/Bitmap (SoftMaskable)", tmpSupport),
+                ("Hidden/TextMeshPro/Distance Field (SoftMaskable)", tmpSupport),
+                ("Hidden/TextMeshPro/Mobile/Distance Field (SoftMaskable)", tmpSupport),
+                // Spine Support
+                ("Hidden/Spine/SkeletonGraphic (SoftMaskable)", "Spine Support"),
+                ("Hidden/Spine/SkeletonGraphic Fill (SoftMaskable)", "Spine Support"),
+                ("Hidden/Spine/SkeletonGraphic Grayscale (SoftMaskable)", "Spine Support"),
+                ("Hidden/Spine/SkeletonGraphic Multiply (SoftMaskable)", "Spine Support"),
+                ("Hidden/Spine/SkeletonGraphic Screen (SoftMaskable)", "Spine Support")
+            });
+        }
+
+        private void Refresh()
+        {
+            m_ShaderVariantRegistry.ClearCache();
+            MaterialRepository.Clear();
+            foreach (var c in Misc.FindObjectsOfType<SoftMaskable>()
+                         .Concat(Misc.GetAllComponentsInPrefabStage<SoftMaskable>()))
+            {
+                c.SetMaterialDirty();
+            }
+
+            EditorApplication.QueuePlayerLoopUpdate();
+        }
+
+        private void Reset()
+        {
+            m_ShaderVariantRegistry.InitializeIfNeeded(this, "(SoftMaskable)");
+        }
+
+        private static void ResetAllHideFlags<T>(HideFlags flags) where T : Component
+        {
+            foreach (var component in Misc.FindObjectsOfType<T>())
+            {
+                component.hideFlags = flags;
+                EditorUtility.SetDirty(component);
+            }
+        }
+
+        [SettingsProvider]
+        private static SettingsProvider CreateSettingsProvider()
+        {
+            return new PreloadedProjectSettingsProvider("Project/UI/Soft Mask");
+        }
+
+        private class Postprocessor : AssetPostprocessor
+        {
+            private static void OnPostprocessAllAssets(string[] _, string[] __, string[] ___, string[] ____)
+            {
+                if (Misc.isBatchOrBuilding) return;
+
+                instance.Refresh();
+            }
+        }
+#endif
+    }
+}
---
+++ _Template/Packages/SoftMaskForUGUI/Runtime/Utilities/SoftMaskUtils.cs
@@ -0,0 +1,235 @@
+using Coffee.UISoftMaskInternal;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+#if URP_ENABLE
+using UnityEngine.Rendering.Universal;
+#endif
+#if TMP_ENABLE
+using TMPro;
+#endif
+
+namespace Coffee.UISoftMask
+{
+    /// <summary>
+    /// Utility class containing various functions and resources for soft masking.
+    /// </summary>
+    internal static class SoftMaskUtils
+    {
+        /// <summary>
+        /// Object pool for CommandBuffer instances.
+        /// </summary>
+        public static readonly InternalObjectPool<CommandBuffer> commandBufferPool =
+            new InternalObjectPool<CommandBuffer>(
+                () => new CommandBuffer(),
+                x => x != null,
+                x => x.Clear());
+
+        /// <summary>
+        /// Object pool for MaterialPropertyBlock instances.
+        /// </summary>
+        public static readonly InternalObjectPool<MaterialPropertyBlock> materialPropertyBlockPool =
+            new InternalObjectPool<MaterialPropertyBlock>(
+                () => new MaterialPropertyBlock(),
+                x => x != null,
+                x => x.Clear());
+
+        private static Material s_SoftMaskingMaterialAdd;
+        private static Material s_SoftMaskingMaterialSub;
+        private static readonly int s_SoftMaskableStereo = Shader.PropertyToID("_SoftMaskableStereo");
+        private static readonly int s_SoftMaskOutsideColor = Shader.PropertyToID("_SoftMaskOutsideColor");
+        private static readonly int s_SoftMaskTex = Shader.PropertyToID("_SoftMaskTex");
+        private static readonly int s_SoftMaskColor = Shader.PropertyToID("_SoftMaskColor");
+        private static readonly int s_MainTex = Shader.PropertyToID("_MainTex");
+        private static readonly int s_ColorMask = Shader.PropertyToID("_ColorMask");
+        private static readonly int s_BlendOp = Shader.PropertyToID("_BlendOp");
+        private static readonly int s_ThresholdMin = Shader.PropertyToID("_ThresholdMin");
+        private static readonly int s_ThresholdMax = Shader.PropertyToID("_ThresholdMax");
+        private static readonly int s_RenderScale = Shader.PropertyToID("_RenderScale");
+        private static readonly int s_DynamicResolutionScale = Shader.PropertyToID("_DynamicResolutionScale");
+        private static float s_CurrentRenderScale = 1;
+        private static Vector2 s_CurrentDynamicResolutionScale = Vector2.one;
+
+#if UNITY_EDITOR
+        [InitializeOnLoadMethod]
+#else
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+#endif
+        private static void InitializeOnLoadMethod()
+        {
+            s_CurrentRenderScale = 1;
+            Shader.SetGlobalFloat(s_RenderScale, s_CurrentRenderScale);
+#if URP_ENABLE
+            UIExtraCallbacks.onBeforeCanvasRebuild += () =>
+            {
+                // Discard variations lesser than 0.05f.
+                var renderScale = 1f;
+                if (GraphicsSettings.currentRenderPipeline is UniversalRenderPipelineAsset urpAsset
+                    && 0.05f < Mathf.Abs(urpAsset.renderScale - 1))
+                {
+                    renderScale = urpAsset.renderScale;
+                }
+
+                if (!Mathf.Approximately(s_CurrentRenderScale, renderScale))
+                {
+                    s_CurrentRenderScale = renderScale;
+                    Shader.SetGlobalFloat(s_RenderScale, s_CurrentRenderScale);
+                }
+            };
+#endif
+            UIExtraCallbacks.onBeforeCanvasRebuild += () =>
+            {
+                var s = new Vector2(ScalableBufferManager.widthScaleFactor, ScalableBufferManager.heightScaleFactor);
+                s.x = Mathf.Clamp(Mathf.CeilToInt(s.x / 0.05f) * 0.05f, 0.25f, 1.0f);
+                s.y = Mathf.Clamp(Mathf.CeilToInt(s.y / 0.05f) * 0.05f, 0.25f, 1.0f);
+
+#if URP_ENABLE
+                if (GraphicsSettings.currentRenderPipeline is UniversalRenderPipelineAsset urpAsset
+                    && Mathf.Abs(urpAsset.renderScale - 1) <= 0.05f)
+                {
+                    s = Vector2.one;
+                }
+#endif
+
+                if (s_CurrentDynamicResolutionScale != s)
+                {
+                    s_CurrentDynamicResolutionScale = s;
+                    Shader.SetGlobalVector(s_DynamicResolutionScale, s_CurrentDynamicResolutionScale);
+                }
+            };
+
+#if TMP_ENABLE
+            TMPro_EventManager.TEXT_CHANGED_EVENT.Add(UpdateMeshUI);
+#endif
+        }
+
+        public static void UpdateMeshUI(Object obj)
+        {
+#if TMP_ENABLE
+            if (!(obj is TextMeshProUGUI text)) return;
+
+            if (text.TryGetComponent<SoftMask>(out var sm))
+            {
+                (sm as IMeshModifier).ModifyMesh(text.mesh);
+                UpdateSubMeshUI(text, sm.enabled, sm.showMaskGraphic, sm.antiAliasingThreshold, sm.softnessRange,
+                    MaskingShape.MaskingMethod.Additive);
+            }
+            else if (text.TryGetComponent<MaskingShape>(out var ms))
+            {
+                (ms as IMeshModifier).ModifyMesh(text.mesh);
+                UpdateSubMeshUI(text, ms.enabled, ms.showMaskGraphic, ms.antiAliasingThreshold, ms.softnessRange,
+                    ms.maskingMethod);
+            }
+        }
+
+        private static void UpdateSubMeshUI(TextMeshProUGUI text, bool enabled, bool show, float aa, MinMax01 softness,
+            MaskingShape.MaskingMethod method)
+        {
+            var subMeshes = InternalListPool<TMP_SubMeshUI>.Rent();
+            text.GetComponentsInChildren(subMeshes, 1);
+
+            for (var i = 0; i < subMeshes.Count; i++)
+            {
+                var maskingShape = subMeshes[i].GetOrAddComponent<MaskingShape>();
+                maskingShape.hideFlags = HideFlags.NotEditable;
+                maskingShape.enabled = enabled;
+                maskingShape.maskingMethod = method;
+                maskingShape.antiAliasingThreshold = aa;
+                maskingShape.softnessRange = softness;
+                maskingShape.showMaskGraphic = show;
+                (maskingShape as IMeshModifier).ModifyMesh(subMeshes[i].mesh);
+            }
+
+            InternalListPool<TMP_SubMeshUI>.Return(ref subMeshes);
+        }
+#else
+        }
+#endif
+
+        /// <summary>
+        /// Applies properties to a MaterialPropertyBlock for soft masking.
+        /// </summary>
+        public static void ApplyMaterialPropertyBlock(MaterialPropertyBlock mpb, int depth, Texture texture,
+            MinMax01 threshold, float alpha)
+        {
+            Profiler.BeginSample("(SM4UI)[SoftMaskUtils] ApplyMaterialPropertyBlock");
+            var colorMask = Vector4.zero;
+            colorMask[depth] = alpha;
+            mpb.SetVector(s_ColorMask, colorMask);
+            mpb.SetTexture(s_MainTex, texture ? texture : null);
+            mpb.SetFloat(s_ThresholdMin, threshold.min);
+            mpb.SetFloat(s_ThresholdMax, threshold.max);
+            Profiler.EndSample();
+        }
+
+        /// <summary>
+        /// Gets the soft masking material based on the masking method.
+        /// </summary>
+        public static Material GetSoftMaskingMaterial(MaskingShape.MaskingMethod method)
+        {
+            return method == MaskingShape.MaskingMethod.Additive
+                ? GetSoftMaskingMaterial(ref s_SoftMaskingMaterialAdd, BlendOp.Add)
+                : GetSoftMaskingMaterial(ref s_SoftMaskingMaterialSub, BlendOp.ReverseSubtract);
+        }
+
+        /// <summary>
+        /// Gets or creates the soft masking material with a specific blend operation.
+        /// </summary>
+        private static Material GetSoftMaskingMaterial(ref Material mat, BlendOp op)
+        {
+            if (mat) return mat;
+
+            mat = new Material(Shader.Find("Hidden/UI/SoftMask"))
+            {
+                hideFlags = HideFlags.DontSave | HideFlags.NotEditable
+            };
+            mat.SetInt(s_BlendOp, (int)op);
+
+#if UNITY_EDITOR
+            UISoftMaskProjectSettings.shaderRegistry.RegisterVariant(mat, "UI > Soft Mask");
+#endif
+            return mat;
+        }
+
+        public static Material CreateSoftMaskable(
+            Material baseMat,
+            Texture softMaskBuffer,
+            int softMaskDepth,
+            int stencilBits,
+            bool isStereo)
+        {
+            Profiler.BeginSample("(SM4UI)[SoftMaskableMaterial] Create > Create New Material");
+            var mat = new Material(baseMat)
+            {
+                shader = UISoftMaskProjectSettings.shaderRegistry.FindOptionalShader(baseMat.shader,
+                    "(SoftMaskable)",
+                    "Hidden/{0} (SoftMaskable)",
+                    "Hidden/UI/Default (SoftMaskable)"),
+                hideFlags = HideFlags.HideAndDontSave
+            };
+            Profiler.EndSample();
+
+            Profiler.BeginSample("(SM4UI)[SoftMaskableMaterial] Create > Set Properties");
+            mat.SetTexture(s_SoftMaskTex, softMaskBuffer);
+            mat.SetInt(s_SoftMaskableStereo, isStereo ? 1 : 0);
+            mat.SetVector(s_SoftMaskColor, new Vector4(
+                0 <= softMaskDepth ? 1 : 0,
+                1 <= softMaskDepth ? 1 : 0,
+                2 <= softMaskDepth ? 1 : 0,
+                3 <= softMaskDepth ? 1 : 0
+            ));
+            mat.EnableKeyword("SOFTMASKABLE");
+            Profiler.EndSample();
+
+#if UNITY_EDITOR
+            UISoftMaskProjectSettings.shaderRegistry.RegisterVariant(mat, "UI > Soft Mask");
+            mat.EnableKeyword("SOFTMASK_EDITOR");
+            mat.SetVector(s_SoftMaskOutsideColor,
+                UISoftMaskProjectSettings.useStencilOutsideScreen ? Vector4.one : Vector4.zero);
+#endif
+            return mat;
+        }
+    }
+}
